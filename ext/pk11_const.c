818adb9de859ccf69fcb0b1ec260b91df8b98595
Lars Kanis <kanis@comcard.de>
2011-03-10 09:17:07 +0100

    refacturing of C code to better allow vendor specific PKCS11-extensions

--- ext/pk11_const.c
+++ ext/pk11_const.c
@@ -1,32 +1,8 @@
 #include "pk11.h"
+#include "pk11_const_macros.h"
 
-#define PKCS11_DEFINE_CONST(constant) \
-  rb_define_const(cPKCS11, #constant, INT2NUM(constant))
-
-#define PKCS11_DEFINE_CONST_GROUP(group, name, value) \
-  do { \
-    VALUE rvalue = ULONG2NUM(value); \
-    rb_define_const(cPKCS11, name, rvalue); \
-    VALUE str = rb_obj_freeze(rb_str_new2(name)); \
-    VALUE old = rb_hash_aref(group, rvalue); \
-    if (!NIL_P(old)) rb_warning("%s is equal to %s", RSTRING_PTR(old), name); \
-    rb_hash_aset(group, rvalue, str); \
-  } while(0)
-
-#define PKCS11_DEFINE_OBJECT_CLASS(constant) \
-  PKCS11_DEFINE_CONST_GROUP(vOBJECT_CLASSES, #constant, constant)
-#define PKCS11_DEFINE_ATTRIBUTE(constant) \
-  PKCS11_DEFINE_CONST_GROUP(vATTRIBUTES, #constant, constant)
-#define PKCS11_DEFINE_MECHANISM(constant) \
-  PKCS11_DEFINE_CONST_GROUP(vMECHANISMS, #constant, constant)
-#define PKCS11_DEFINE_RETURN_VALUE(constant) \
-  do { \
-    VALUE eError = rb_define_class_under(cPKCS11, #constant, ePKCS11Error); \
-    VALUE rvalue = ULONG2NUM(constant); \
-    VALUE old = rb_hash_aref(vRETURN_VALUES, rvalue); \
-    if (!NIL_P(old)) rb_warning("%s is equal to %s", RSTRING_PTR(old), #constant); \
-    rb_hash_aset(vRETURN_VALUES, rvalue, eError); \
-  } while(0)
+#define MODULE_FOR_CONSTS cPKCS11
+#define BASECLASS_FOR_ERRORS ePKCS11Error
 
 static VALUE vOBJECT_CLASSES;
 static VALUE vATTRIBUTES;
